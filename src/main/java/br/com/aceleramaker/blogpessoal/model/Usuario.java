f26a4bb0715df8f0fafb5dcc0505f75a0915d12f
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-07 08:58:17 -0300

    feat: mapeamento inicial entidade Usuario

---
+++ src/main/java/br/com/aceleramaker/blogpessoal/model/Usuario.java
@@ -0,0 +1,69 @@
+package br.com.aceleramaker.blogpessoal.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String usuario;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column
+    private String foto;
+
+    public Usuario(String nome, String usuario, String senha, String foto) {
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.foto = foto;
+    }
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
