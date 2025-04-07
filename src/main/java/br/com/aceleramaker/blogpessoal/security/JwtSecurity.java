fb4fa448f300903fc460b61bb8367008cafc03a1
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-07 19:14:00 -0300

    feat: geração, validação e extração de token JWT

---
+++ src/main/java/br/com/aceleramaker/blogpessoal/security/JwtSecurity.java
@@ -0,0 +1,49 @@
+package br.com.aceleramaker.blogpessoal.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
+public class JwtSecurity {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String gerarTokenJwt(String usuario) {
+        return Jwts.builder()
+                .setSubject(usuario)
+                .setIssuedAt(new Date())
+                .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extrairUsuario(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validarToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
