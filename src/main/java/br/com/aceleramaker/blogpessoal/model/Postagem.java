e56b6ec731356fa8560384a8c1492a30f8b917f7
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-07 19:28:00 -0300

    feat: método setId na entidade Postagem

--- src/main/java/br/com/aceleramaker/blogpessoal/model/Postagem.java
+++ src/main/java/br/com/aceleramaker/blogpessoal/model/Postagem.java
@@ -47,6 +47,10 @@ public class Postagem {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitulo() {
         return titulo;
     }
